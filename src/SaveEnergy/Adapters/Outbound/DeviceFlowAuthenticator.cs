bbb5f2d2fce63ea043feb163f7f2aa7a7271abad
Stefan Boos <kontakt@boos.systems>
2024-09-07 06:11:37 +0200

    style(DeviceFlowAuthenticator): dotnet csharpier ./src/SaveEnergy/Adapters/Outbound/DeviceFlowAuthenticator.cs

--- src/SaveEnergy/Adapters/Outbound/DeviceFlowAuthenticator.cs
+++ src/SaveEnergy/Adapters/Outbound/DeviceFlowAuthenticator.cs
@@ -14,27 +14,33 @@ internal class DeviceFlowAuthenticator : ICanAuthenticate
     private readonly IConfiguration _configuration;
     private readonly ILogger<DeviceFlowAuthenticator> _logger;
 
-    public DeviceFlowAuthenticator(IHttpClientFactory httpClientFactory,
+    public DeviceFlowAuthenticator(
+        IHttpClientFactory httpClientFactory,
         IConfiguration configuration,
-        ILogger<DeviceFlowAuthenticator> logger)
+        ILogger<DeviceFlowAuthenticator> logger
+    )
     {
         _httpClientFactory = httpClientFactory;
         _configuration = configuration;
         _logger = logger;
-        
+
         _authenticationClient = CreateAuthenticationClient();
     }
 
     private HttpClient CreateAuthenticationClient()
     {
         var authenticationClient = _httpClientFactory.CreateClient();
-        
+
         authenticationClient.DefaultRequestHeaders.Add("Accept", "application/json");
-        authenticationClient.BaseAddress =
-            new Uri(_configuration["GitHub:AuthenticationBaseAddress"] ?? "https://github.com");
-        
-        _logger.LogDebug("Authentication base address: {AuthenticationBaseAddress}", authenticationClient.BaseAddress);
-        
+        authenticationClient.BaseAddress = new Uri(
+            _configuration["GitHub:AuthenticationBaseAddress"] ?? "https://github.com"
+        );
+
+        _logger.LogDebug(
+            "Authentication base address: {AuthenticationBaseAddress}",
+            authenticationClient.BaseAddress
+        );
+
         return authenticationClient;
     }
 
@@ -42,46 +48,55 @@ internal class DeviceFlowAuthenticator : ICanAuthenticate
     {
         var deviceCodeResponse = await RequestDeviceCode();
 
-        Console.WriteLine("Please visit {0} and enter the code \"{1}\" to authenticate this application.",
-            deviceCodeResponse.VerificationUri, deviceCodeResponse.UserCode);
+        Console.WriteLine(
+            "Please visit {0} and enter the code \"{1}\" to authenticate this application.",
+            deviceCodeResponse.VerificationUri,
+            deviceCodeResponse.UserCode
+        );
 
         var accessTokenResponse = await WaitUntilAccessGranted(deviceCodeResponse);
-        
+
         return new AccessToken(accessTokenResponse.AccessToken);
     }
 
     private async Task<DeviceCodeResponse> RequestDeviceCode()
     {
-        var deviceCodeResponse = await PostJsonAsync<DeviceCodeResponse>("/login/device/code", new
-        {
-            client_id = _configuration["GitHub:ClientId"],
-        });
-        
+        var deviceCodeResponse = await PostJsonAsync<DeviceCodeResponse>(
+            "/login/device/code",
+            new { client_id = _configuration["GitHub:ClientId"], }
+        );
+
         _logger.LogDebug("Device code: {DeviceCodeResponse}", deviceCodeResponse);
-        
+
         return deviceCodeResponse;
     }
 
     private async Task<TResult> PostJsonAsync<TResult>(string requestUri, object value)
     {
         using var httpResponse = await _authenticationClient.PostAsJsonAsync(requestUri, value);
-        
+
         httpResponse.EnsureSuccessStatusCode();
 
         return await httpResponse.Content.ReadFromJsonAsync<TResult>();
     }
 
-    private async Task<AccessTokenResponse> WaitUntilAccessGranted(DeviceCodeResponse deviceCodeResponse)
+    private async Task<AccessTokenResponse> WaitUntilAccessGranted(
+        DeviceCodeResponse deviceCodeResponse
+    )
     {
         var secondsPassed = Stopwatch.StartNew();
-        
+
         var accessTokenResponse = new AccessTokenResponse();
-        while (secondsPassed.Elapsed.TotalSeconds < deviceCodeResponse.ExpiresIn &&
-               string.IsNullOrEmpty(accessTokenResponse.AccessToken))
+        while (
+            secondsPassed.Elapsed.TotalSeconds < deviceCodeResponse.ExpiresIn
+            && string.IsNullOrEmpty(accessTokenResponse.AccessToken)
+        )
         {
             Thread.Sleep(deviceCodeResponse.Interval * 1000);
-            _logger.LogDebug("Checking for authentication success for another {0} minutes ...",
-                (deviceCodeResponse.ExpiresIn - secondsPassed.Elapsed.TotalSeconds) / 60);
+            _logger.LogDebug(
+                "Checking for authentication success for another {0} minutes ...",
+                (deviceCodeResponse.ExpiresIn - secondsPassed.Elapsed.TotalSeconds) / 60
+            );
 
             accessTokenResponse = await RequestAccessToken(deviceCodeResponse);
         }
@@ -90,14 +105,19 @@ internal class DeviceFlowAuthenticator : ICanAuthenticate
         return accessTokenResponse;
     }
 
-    private async Task<AccessTokenResponse> RequestAccessToken(DeviceCodeResponse deviceCodeResponse)
+    private async Task<AccessTokenResponse> RequestAccessToken(
+        DeviceCodeResponse deviceCodeResponse
+    )
     {
-        var accessTokenResponse = await PostJsonAsync<AccessTokenResponse>("/login/oauth/access_token", new
+        var accessTokenResponse = await PostJsonAsync<AccessTokenResponse>(
+            "/login/oauth/access_token",
+            new
             {
                 client_id = _configuration["GitHub:ClientId"],
                 device_code = deviceCodeResponse.DeviceCode,
                 grant_type = "urn:ietf:params:oauth:grant-type:device_code",
-            });
+            }
+        );
 
         _logger.LogDebug("Access token: {AccessTokenResponse}", accessTokenResponse);
 
@@ -111,13 +131,13 @@ internal class DeviceFlowAuthenticator : ICanAuthenticate
 
         [JsonPropertyName("user_code")]
         public string UserCode { get; init; }
-    
+
         [JsonPropertyName("verification_uri")]
         public string VerificationUri { get; init; }
-    
+
         [JsonPropertyName("expires_in")]
         public int ExpiresIn { get; init; }
-    
+
         [JsonPropertyName("interval")]
         public int Interval { get; init; }
     }
@@ -126,16 +146,16 @@ internal class DeviceFlowAuthenticator : ICanAuthenticate
     {
         [JsonPropertyName("access_token")]
         public string AccessToken { get; init; }
-    
+
         [JsonPropertyName("expires_in")]
         public int ExpiresIn { get; init; }
-    
+
         [JsonPropertyName("refresh_token")]
         public string RefreshToken { get; init; }
-    
+
         [JsonPropertyName("token_type")]
         public string TokenType { get; init; }
-    
+
         [JsonPropertyName("scope")]
         public string Scope { get; init; }
     }
